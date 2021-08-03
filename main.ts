d94ce0d02acc8efaf10425a1f8268483619f1b5e
sljm12 <leejianm@hotmail.com>
2021-08-03 11:25:36 +0800

    Added bunch of functions to detect which button is pressed and also to ensure that only when button is up then we detect it

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function pressed () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        return 0
+    } else {
+        return -1
+    }
+}
 function showLightSound (num: number) {
     if (num == 0) {
         music.playTone(349, music.beat(BeatFraction.Whole))
@@ -13,6 +20,37 @@ function showLightSound (num: number) {
         music.rest(music.beat(BeatFraction.Whole))
     }
 }
+function isPinPressed (num: number) {
+    pin = getPin(num)
+    if (pin == 0) {
+        while (pins.digitalReadPin(DigitalPin.P0) != 1) {
+        	
+        }
+        return 1
+    } else {
+        return 0
+    }
+}
+function getPin (num: number) {
+    if (num == 0) {
+        return pins.digitalReadPin(DigitalPin.P0)
+    } else if (num == 1) {
+        return pins.digitalReadPin(DigitalPin.P1)
+    } else if (num == 2) {
+        return pins.digitalReadPin(DigitalPin.P2)
+    } else if (num == 3) {
+        return pins.digitalReadPin(DigitalPin.P2)
+    } else {
+        return -1
+    }
+}
+function whichButtonPressed () {
+    if (isPinPressed(0) == 1) {
+        return 0
+    } else {
+        return -1
+    }
+}
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
@@ -26,9 +64,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.compassHeading())
 })
-input.onGesture(Gesture.Shake, function () {
-	
-})
+let pin = 0
 basic.showLeds(`
     # . . . #
     . # # # .
@@ -36,10 +72,11 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
+led.enable(false)
 let correct_sequence = [randint(0, 3)]
-for (let value of correct_sequence) {
-    showLightSound(value)
-}
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 basic.forever(function () {
-	
+    serial.writeNumber(whichButtonPressed())
+    serial.writeLine("")
+    basic.pause(100)
 })
